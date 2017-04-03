d1ad4f6d05a22ed4ce7c472fd4d0140b4296c51e
Adriano T. Tanaka <adritanaka@gmail.com>
2017-04-02 23:00:39 -0300

    Inclusão do validador de CEP

--- cep-service/src/main/java/exercise/business/CepServiceBusiness.java
+++ cep-service/src/main/java/exercise/business/CepServiceBusiness.java
@@ -1,10 +1,26 @@
 package exercise.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import exercise.business.service.CepValidationService;
 import exercise.model.Address;
 
+@Service
 public class CepServiceBusiness {
 
+	@Autowired
+	private CepValidationService validationService; 
+	
 	public Address searchAddress(String cep) {
+		// validar se o cep esta certo
+		
+		// caso não seja valido retornar erro - CEP inválido
+		
+		// caso esteja certo buscar os dados do CEP correspondente
+		// caso esteja certo mas não encontre o CEP incluir 0 (zeros) a esquerda até encontrar (isso em qto houver itens)
+		
+		
 		return null;
 	}
 
---
+++ cep-service/src/main/java/exercise/business/service/CepValidatior.java
@@ -0,0 +1,8 @@
+package exercise.business.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CepValidationService {
+
+}
