a91bad8673e39020c4e4cd7f31fd691308356f22
Adriano T. Tanaka <adritanaka@gmail.com>
2017-04-03 23:07:09 -0300

    Criação do Repository "mockado"

--- cep-service/src/main/java/exercise/entity/CepDetails.java
+++ cep-service/src/main/java/exercise/entity/CepDetails.java
@@ -2,4 +2,64 @@ package exercise.entity;
 
 public class CepDetails {
 
+	private Long id;
+	
+	private String street;
+	
+	private String district;
+	
+	private String city;
+	
+	private String state;
+
+	private String cep;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
 }
--- cep-service/src/main/java/exercise/repository/CepServiceRepository.java
+++ cep-service/src/main/java/exercise/repository/CepServiceRepository.java
@@ -1,14 +1,42 @@
 package exercise.repository;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.stereotype.Repository;
 
+import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
 import exercise.entity.CepDetails;
 
 @Repository
 public class CepServiceRepository {
 
+	public CepServiceRepository() {
+		FixtureFactoryLoader.loadTemplates("exercise.factory.template");
+	}
+	
 	public CepDetails getDetails(String cep) {
-		return null;
+		CepDetails cepDetails = search(cep);
+		
+		return cepDetails;
+	}
+
+	/**
+	 * Método mock para busca do CEP
+	 * @param cep
+	 * @return {@link CepDetails}
+	 */
+	private CepDetails search(String cep) {
+		List<CepDetails> cepList = Fixture.from(CepDetails.class)
+				.gimme(3, Arrays.asList("paulista", "candido-de-abreu", "estadio-maracana"));
+		
+		try {
+			return cepList.stream().filter(cepDetails -> cep.equals(cepDetails.getCep())).findFirst().get();
+		} catch (NullPointerException | NoSuchElementException e) {
+			return null;
+		}
 	}
 	
 }
