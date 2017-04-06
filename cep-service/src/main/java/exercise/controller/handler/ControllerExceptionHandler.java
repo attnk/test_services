42507f6e44f1adf5890c47f4ecd40714deb8223c
Adriano T. Tanaka <adritanaka@gmail.com>
2017-04-05 21:59:54 -0300

    Ajuste no cep service
    Ajustado para conter somente serviço referente a BUSCA e validação do CEP

--- cep-service/src/main/java/exercise/controller/handler/ControllerExceptionHandler.java
+++ cep-service/src/main/java/exercise/controller/handler/ControllerExceptionHandler.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import exercise.exception.CouldNotConvertException;
-import exercise.exception.CouldNotFoundAdressException;
-import exercise.exception.CouldNotProcessException;
-import exercise.exception.CouldNotProcessInvalidArgumentException;
 import exercise.exception.InvalidCepException;
 import exercise.exception.NotFoundCepException;
 import exercise.model.ProcessMessage;
@@ -20,8 +16,7 @@ public class ControllerExceptionHandler {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)  
     @ExceptionHandler({
-    	InvalidCepException.class, 
-    	CouldNotProcessInvalidArgumentException.class
+    	InvalidCepException.class
     })
 	public ProcessMessage handleInvalidCep(Exception e) {
 		ProcessMessage response = new ProcessMessage();
@@ -32,24 +27,11 @@ public class ControllerExceptionHandler {
 	
 	@ResponseStatus(HttpStatus.NOT_FOUND)  
     @ExceptionHandler({
-    	NotFoundCepException.class,
-    	CouldNotFoundAdressException.class
+    	NotFoundCepException.class
     })
 	public ProcessMessage handleNotFoundCep(Exception e) {
 		ProcessMessage response = new ProcessMessage();
 		response.setMessage(e.getMessage());
-		
-		return response;
-	}
-	
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  
-    @ExceptionHandler({
-    	CouldNotProcessException.class,
-    	CouldNotConvertException.class
-    })
-	public ProcessMessage handleInternalError() {
-		ProcessMessage response = new ProcessMessage();
-		response.setMessage(String.format("Erro ao realizar o processamento!"));
 		
 		return response;
 	}
