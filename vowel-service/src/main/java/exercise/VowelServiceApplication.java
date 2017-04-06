d759b609a5964c9e49c7c2af9f70a96216347956
Adriano T. Tanaka <adritanaka@gmail.com>
2017-04-05 21:35:10 -0300

    Inclusão dos projetos address e vowel

---
+++ vowel-service/src/main/java/exercise/VowelServiceApplication.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VowelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VowelServiceApplication.class, args);
+	}
+}
